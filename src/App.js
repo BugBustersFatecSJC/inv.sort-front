f747f1537fc8ec6c4b7a13c236c246a871ebbc21
danprsp <daniel.piloto@fatec.sp.gov.br>
2024-10-17 21:07:49 -0300

    [feat]: rotas do movements

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import InitialSignUp from './pages/InitialSignUp/InitialSignUp'
 import api from "./services/api"
 // import UserPage from './pages/UserPage'
 import MainPageRender from './pages/MainPageRender/MainPageRender'
+import BuyAndSell from './pages/BuyAndSell/BuyAndSell';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" element={<InitialSignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/products" element={<MainPageRender />} />
+          <Route path="/movements" element={<BuyAndSell/>} />
           {/* <Route path="/userpage" element={<UserPage />} /> */}
         </Routes>
       </Router>
